aa8bcec5692e18222288791314f11b0d7a7972ff
MM <manufacturevn@gmail.com>
2021-03-15 12:51:00 +0700

    add add_markers_static

--- src/scripts/add_marker.sh
+++ src/scripts/add_marker.sh
@@ -4,4 +4,4 @@ xterm -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5 
-xterm -e  " rosrun add_markers add_markers_node"
+xterm -e  " rosrun add_markers add_markers_static_node"
